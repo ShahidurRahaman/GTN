3878484a7c18487fbefd483605cc39f0b5afaebe
Sk Shahidur Rahaman <116867117+ShahidurRahaman@users.noreply.github.com>
2024-03-04 00:44:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
-let a;
+let a,count=0;
 function generate(){
     a=Math.floor(Math.random() * 101);
 }
 function check(){
+    count++;
     let b=document.getElementById("input");
     b=parseInt(b.value , 10);
-    if(b==a)
+    if(b==a){
         document.getElementById("output-text").innerHTML="<p style='color: rgb(11, 241, 11);'>Hurray! You've Guessed The Secret Number</p>";
-    else if(b<a)
+        document.getElementById("input").style.display="none";
+        document.getElementById("pointbox").style.display="inline-block";
+        document.getElementById("pointbox").innerHTML="Points: "+(100-5*(count-1));
+    }
+    else if(b<a){
         document.getElementById("output-text").innerHTML="<p style='color: rgb(248, 43, 43);'>Secret Number is greater than your guess</p>";
-    else 
+    }
+    else{
         document.getElementById("output-text").innerHTML="<p style='color: rgb(248, 43, 43);'>Secret Number is smaller than your guess</p>";
+    }
 }
 function newgame(){
     window.location.href="./index.html";
